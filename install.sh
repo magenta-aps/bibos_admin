474fb83fbf5318ad2bf1f43545fce35c70ad4f7b
Carsten Agger <carstena@magenta-aps.dk>
2013-05-07 15:00:59 +0200

    First version of the models/object structure in /system/.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Setup virtualenv, install packages necessary to run BibOS Admin.
+# System requirements, installed packages etc. should be checked 
+# in a separate Dependencies script which should be combined with this one.
+
+virtualenv .
+source bin/activate
+pip install django
+
+
