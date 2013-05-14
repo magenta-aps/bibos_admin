31265a4a2cc79d45b500f314098bb30ccf713519
Carsten Agger <carstena@magenta-aps.dk>
2013-05-14 12:01:00 +0200

    Make admin work satisfactorily for system app, first approximation.

--- admin_site/system/admin.py
+++ admin_site/system/admin.py
@@ -6,11 +6,40 @@ from models import Site, Distribution, PCGroup, PC
 
 ar = admin.site.register
 
-ar(Configuration)
-ar(ConfigurationEntry)
-ar(PackageList)
+class ConfigurationEntryInline(admin.TabularInline):
+    model = ConfigurationEntry
+    extra = 3
+
+
+class PackageInline(admin.TabularInline):
+    model = Package
+    # Don't include a lot of extras, the intention is not that these lists
+    # should be updated manually.
+    extra = 1
+
+
+class PackageListAdmin(admin.ModelAdmin):
+    fields = ['name', 'uid']
+    inlines = [PackageInline]
+
+
+class ConfigurationAdmin(admin.ModelAdmin):
+    fields = ['name']
+    inlines = [ConfigurationEntryInline]
+
+
+class PCInline(admin.TabularInline):
+    model = PC.pc_groups.through
+    extra = 3
+
+
+class PCGroupAdmin(admin.ModelAdmin):
+    inlines = [PCInline]
+
+
+ar(Configuration, ConfigurationAdmin)
+ar(PackageList, PackageListAdmin)
 ar(Site)
 ar(Distribution)
-ar(Package)
-ar(PCGroup)
+ar(PCGroup, PCGroupAdmin)
 ar(PC)
--- admin_site/system/models.py
+++ admin_site/system/models.py
@@ -4,12 +4,16 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
 
+
 class Configuration(models.Model):
     """This class contains/represents the configuration of a Site, a
     Distribution, a PC Group or a PC."""
     # Doesn't need any actual fields, it seems. Should not exist independently
     # of the classes to which it may be aggregated.
-    pass
+    name = models.CharField(max_length=255, unique=True)
+
+    def __unicode__(self):
+        return self.name
 
 
 class ConfigurationEntry(models.Model):
@@ -29,31 +33,39 @@ class PackageList(models.Model):
     name = models.CharField(_('name'), max_length=255)
     uid = models.CharField(_('id'), max_length=255)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Site(models.Model):
     """A site which we wish to admin"""
     name = models.CharField(_('name'), max_length=255)
-    uid = models.CharField(_('id'), max_length=255)  # Unique ID, not editable
+    uid = models.CharField(_('uid'), max_length=255, unique=True)  # Unique ID, not editable
     users = models.ManyToManyField(User,
                                    related_name='site_users',
                                    verbose_name=_('Site Users'),
                                    blank=True)
     configuration = models.ForeignKey(Configuration)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Distribution(models.Model):
     """This represents a GNU/Linux distribution managed by us."""
     name = models.CharField(_('name'), max_length=255)
-    uid = models.CharField(_('id'), max_length=255)
+    uid = models.CharField(_('uid'), max_length=255)
     configuration = models.ForeignKey(Configuration)
     package_list = models.ForeignKey(PackageList)
 
+    def __unicode__(self): 
+        return self.name
 
 
 class Package(models.Model):
     """This class represents a single Debian package to be installed."""
     name = models.CharField(_('name'), max_length=255)
-    uid = models.CharField(_('id'), max_length=255)
+    uid = models.CharField(_('uid'), max_length=255)
     version = models.CharField(_('version'), max_length=255)
     package_list = models.ForeignKey(PackageList, 
                                      related_name='package_list',
@@ -65,15 +77,23 @@ class PCGroup(models.Model):
     name = models.CharField(_('name'), max_length=255)
     uid = models.CharField(_('id'), max_length=255)
 
+    def __unicode__(self):
+        return self.name
+
+
 class PC(models.Model):
     """This class represents one PC, i.e. one client of the admin system."""
     name = models.CharField(_('name'), max_length=255)
-    uid = models.CharField(_('id'), max_length=255)
-    description = models.CharField(_('description'), max_length=1024)
+    uid = models.CharField(_('uid'), max_length=255)
+    description = models.CharField(_('description'), max_length=1024,
+                                   blank=True)
     distribution = models.ForeignKey(Distribution)
     configuration = models.ForeignKey(Configuration)
-    pc_groups = models.ManyToManyField(PCGroup)
+    pc_groups = models.ManyToManyField(PCGroup, related_name='pcs', blank=True)
     package_list = models.ForeignKey(PackageList)
+
+    def __unicode__(self):
+        return self.name
 
 
 
