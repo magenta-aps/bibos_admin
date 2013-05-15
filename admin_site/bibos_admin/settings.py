c797fcc2712313f3a580e4a5bd05d3984a1dc4bb
Carsten Agger <carstena@magenta-aps.dk>
2013-05-15 08:04:37 +0200

    Test server now working.

--- admin_site/bibos_admin/settings.py
+++ admin_site/bibos_admin/settings.py
@@ -1,5 +1,10 @@
 # Django settings for bibos_admin project.
 
+import os
+
+from wsgi import install_dir, lib_dir
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -9,14 +14,20 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '.database.db',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', 
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(install_dir, '.database.db'),
+        # Or path to database file if using sqlite3.                      
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '',                      
+        # Empty for localhost through domain sockets or '127.0.0.1' for localhost
+        # through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
@@ -50,12 +61,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(install_dir, 'site_media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
--- admin_site/bibos_admin/wsgi.py
+++ admin_site/bibos_admin/wsgi.py
@@ -14,12 +14,21 @@ framework.
 
 """
 import os
+import sys
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "bibos_admin.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bibos_admin.settings")
+
+install_dir = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')
+)
+lib_dir = os.path.join(install_dir, 'lib/python2.7/site-packages')
+
+sys.path[0:0] = [install_dir, lib_dir]
+
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
