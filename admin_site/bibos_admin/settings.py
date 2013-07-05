9fe190ffa8b18daa6b94978e34c7702e06b28975
Carsten Agger <carstena@magenta-aps.dk>
2013-07-05 14:56:00 +0200

    Last seen and update required now displayed. Date localization could be better.

--- admin_site/bibos_admin/settings.py
+++ admin_site/bibos_admin/settings.py
@@ -17,7 +17,6 @@ ADMINS = (
 MANAGERS = ADMINS
 
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', 
@@ -42,11 +41,11 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Copenhagen'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'da-DK'
 
 SITE_ID = 1
 
