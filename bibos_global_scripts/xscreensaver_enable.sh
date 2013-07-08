ff9b39ccc8c7402023d3465257967a971dba84f9
Carsten Agger <carstena@magenta-aps.dk>
2013-07-08 14:24:47 +0200

    Scripts to manage screensavers & related settings.

---
+++ bibos_global_scripts/xscreensaver_enable.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Disable Gnome screensaver, install A LOT of graphic screensavers,
+# enable screensaver for user by default.
+
+
+apt-get remove gnome-screensaver
+apt-get install xscreensaver xscreensaver-data xscreensaver-gl
+
+mkdir -p /home/.skjult/.config/autostart
+
+cat << EOF > /home/.skjult/.config/autostart/xscreensaver.desktop
+
+[Desktop Entry]
+Type=Application
+Exec=xscreensaver -nosplash
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[da_DK]=Pauseskærm
+Name=Pauseskærm
+Comment[da_DK]=
+Comment=
+EOF
+
+exit 0
+
