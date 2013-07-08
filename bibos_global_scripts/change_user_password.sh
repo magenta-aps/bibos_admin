b2d225493cf5b1a5237555e17004e530289ad26c
Carsten Agger <carstena@magenta-aps.dk>
2013-07-08 17:00:15 +0200

    Scripts for changing password.

---
+++ bibos_global_scripts/change_user_password.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash 
+
+# This script will change the audience user password on a BibOS machine.
+
+
+# Expect exactly two input parameters
+
+if [ $# -ne 2 ]
+then
+    echo "usage: $(basename $0) <password> <confirmation>"
+    echo ""
+    exit -1
+fi
+
+if [ "$1" == "$2" ]
+then
+    # change password
+    TARGET_USER=user
+    PASSWORD="$1"
+    CRYPTPASS=$(perl -e 'print crypt($ARGV[0], "password")' $PASSWORD)
+    # Done calculating, now do it.
+    /usr/sbin/usermod $TARGET_USER -p $CRYPTPASS
+else
+    echo "Passwords didn't match!"
+    exit -1
+fi
+
+exit 0
+
+
+
