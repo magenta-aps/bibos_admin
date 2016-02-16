bbb75763d693c63add4f0741010006b778d7b4cf
dals83 <danni@magenta-aps.dk>
2016-02-16 09:40:52 +0100

    Added Remember text.

--- bibos_client/bin/admin_connect.sh
+++ bibos_client/bin/admin_connect.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+# Remember to increase the version number if any changes are made!
 VERSION=1.0
+
 sudo apt-get update
 sudo apt-get upgrade --assume-yes
 sudo apt-get install curl --assume-yes
@@ -9,6 +12,5 @@ sudo pip install netifaces
 sudo pip install lockfile
 sudo pip install bibos-client
 sudo mkdir -p /var/lib/bibos/jobs/
-# Set version in configuration
 sudo set_bibos_config admin_connect_version $VERSION
 sudo register_new_bibos_client.sh
