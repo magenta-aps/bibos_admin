6653395a9354af7753f3daed67e2ce95496577c0
dals83 <danni@magenta-aps.dk>
2016-04-21 10:36:47 +0200

    Initial steps for 'USB keyboard added event' data extraction.

--- admin_site/securityscripts/detectusbevents.sh
+++ admin_site/securityscripts/detectusbevents.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#extract needed data.
+#what is needed data???
+#keywords : keyboard, Product, Manufacturer, input
+
 echo "USB Keyboard attachment is detected." >> /var/log/usbevent.log
 
 tail /var/log/syslog >> /var/log/usbevent.log
+
+python securityKeywordExtractor.py ['Product:', 'Manufacturer:', 'input:']
