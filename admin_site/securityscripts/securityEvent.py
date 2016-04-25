7f397f3f42b6c59fda52c6ba6976722e8e7824e1
dals83 <danni@magenta-aps.dk>
2016-04-25 11:08:35 +0200

    securityEvent.py now reads directly from the syslog and extracts the needed data.

---
+++ admin_site/securityscripts/securityEvent.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+
+import os
+import sys
+import linecache
+
+syslog = "/var/log/syslog"
+totalNumberOfLines = len(open(syslog).readlines())
+
+lines=""
+# use line cache module to read the lines
+for i in range(totalNumberOfLines - 10 + 1, totalNumberOfLines+1):
+     lines += str(linecache.getline(syslog,i),)
+
+securityEventFile = open("/etc/bibos/security/event.txt", "a")
+
+#Ignore first argument
+iterargs = iter(sys.argv)
+next(iterargs)
+for arg in iterargs:    
+        #find keyword
+        #select text from keyword until end of line        
+        before_keyword, keyword, after_keyword = lines.partition(arg)
+        securityEventFile.write(arg)
+        if(after_keyword != ""):
+            splittet_lines = after_keyword.splitlines()
+            if(len(splittet_lines) > 0):        
+                securityEventFile.write(splittet_lines[0]+"\n")
+
+securityEventFile.write(lines+"\n")    
+securityEventFile.close()
