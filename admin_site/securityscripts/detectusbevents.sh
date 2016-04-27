659c88b32bdfe4d4180cfe46c474e66633c892ee
dals83 <danni@magenta-aps.dk>
2016-04-27 10:39:48 +0200

    Improved functionality. The securityEvent.py is now called directly from the .rules file. securityEvent.py now only loads a part of the syslog file.

--- admin_site/securityscripts/detectusbevents.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#extract needed data.
-#keywords : keyboard, Product, Manufacturer, input
-
-sudo /etc/bibos/security/securityEvent.py "Product:" "Manufacturer:" "input:"
--- admin_site/securityscripts/securityEvent.py
+++ admin_site/securityscripts/securityEvent.py
@@ -5,13 +5,25 @@ import sys
 import linecache
 from datetime import datetime
 
-syslog = "/var/log/syslog"
-totalNumberOfLines = len(open(syslog).readlines())
+bufsize = 8192
 
+numberOflines = 10
+fname = "/var/log/syslog"
+fsize = os.stat(fname).st_size
 lines=""
-# use line cache module to read the lines
-for i in range(totalNumberOfLines - 10 + 1, totalNumberOfLines+1):
-     lines += str(linecache.getline(syslog,i),)
+
+syslog_iter = 0
+with open(fname) as f:
+    if bufsize > fsize:
+        bufsize = fsize-1
+    data = []
+    while True:
+        syslog_iter +=1
+        f.seek(fsize-bufsize*syslog_iter)
+        data.extend(f.readlines())
+        if len(data) >= numberOflines or f.tell() == 0:
+            lines += str(''.join(data[-numberOflines:]))
+            break
 
 securityEventFile = open("/etc/bibos/security/event.txt", "a")
 securityEventFile.write("Timestamp: " + datetime.now().strftime('%Y%m%d%H%M') + "\n")
@@ -21,11 +33,11 @@ next(iterargs)
 for arg in iterargs:    
         #find keyword
         #select text from keyword until end of line        
-        before_keyword, keyword, after_keyword = lines.partition(arg)
-        securityEventFile.write(arg)
+        before_keyword, keyword, after_keyword = lines.partition(arg)        
         if(after_keyword != ""):
             splittet_lines = after_keyword.splitlines()
-            if(len(splittet_lines) > 0):        
+            if(len(splittet_lines) > 0):
+                securityEventFile.write(arg)        
                 securityEventFile.write(splittet_lines[0]+"\n")
 
 securityEventFile.write(lines+"\n")    
