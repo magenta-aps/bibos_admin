1ac5e890eab67ce2bc95c6fc4c0a9a2fe5c8ade9
dals83 <danni@magenta-aps.dk>
2016-05-04 12:10:25 +0200

    flake 8 changes.

--- bibos_client/bibos_client/jobmanager.py
+++ bibos_client/bibos_client/jobmanager.py
@@ -4,16 +4,13 @@ import os.path
 import stat
 import urllib2
 import json
-import datetime
 import urlparse
 import glob
 import re
 import subprocess
-import bibos_client.bibos_proxy_setup
 import tempfile
-import fcntl
 
-from datetime import timedelta
+from datetime import datetime
 
 from bibos_utils.bibos_config import BibOSConfig
 
@@ -36,13 +33,15 @@ Directory structure for storing BibOS jobs:
 Directory structure for BibOS security events:
 /etc/bibos/security/securityevent.csv - Security event log file.
 /etc/bibos/security/ - Scripts to be executed by the jobmanager.
-/etc/bibos/security/security_check_YYYYMMDDHHmm.csv - files containing the events to be sent to the admin system.
+/etc/bibos/security/security_check_YYYYMMDDHHmm.csv -
+files containing the events to be sent to the admin system.
 """
 SECURITY_DIR = '/etc/bibos/security'
 JOBS_DIR = '/var/lib/bibos/jobs'
 LOCK = filelock(JOBS_DIR + '/running')
 PACKAGE_LIST_FILE = '/var/lib/bibos/current_packages.list'
 PACKAGE_LINE_MATCHER = re.compile('ii\s+(\S+)\s+(\S+)\s+(.*)')
+
 
 class LocalJob(dict):
     def __init__(self, id=None, path=None, data=None):
@@ -119,11 +118,11 @@ class LocalJob(dict):
         self.save_property_to_file('status', self.status_path)
 
     def mark_started(self):
-        self['started'] = str(datetime.datetime.now())
+        self['started'] = str(datetime.now())
         self.save_property_to_file('started', self.started_path)
 
     def mark_finished(self):
-        self['finished'] = str(datetime.datetime.now())
+        self['finished'] = str(datetime.now())
         self.save_property_to_file('finished', self.finished_path)
 
     def load_local_parameters(self):
@@ -184,7 +183,7 @@ class LocalJob(dict):
             param_fh.close()
 
     def translate_parameters(self):
-        if not 'parameters' in self:
+        if 'parameters' not in self:
             return
 
         config = BibOSConfig()
@@ -261,6 +260,7 @@ class LocalJob(dict):
             log.close()
         else:
             print >> os.sys.stderr, "Will not run job without aquired lock"
+
 
 def get_url_and_uid():
     config = BibOSConfig()
@@ -371,15 +371,15 @@ def get_instructions():
         for j in instructions['jobs']:
             local_job = LocalJob(data=j)
             local_job.save()
-            local_job.logline("Job imported at %s" % datetime.datetime.now())
+            local_job.logline("Job imported at %s" % datetime.now())
 
     if ('do_send_package_info' in instructions and
-        instructions['do_send_package_info']):
-        try:
-            # Send full package info to server.
-            upload_packages()
-        except Exception as e:
-            print >> os.sys.stderr, "Package upload failed" + str(e)
+            instructions['do_send_package_info']):
+                try:
+                    # Send full package info to server.
+                    upload_packages()
+                except Exception as e:
+                    print >> os.sys.stderr, "Package upload failed" + str(e)
 
 
 def check_outstanding_packages():
@@ -426,73 +426,79 @@ def run_pending_jobs():
     else:
         print >> os.sys.stderr, "Aquire the lock before running jobs"
 
-def collect_security_events(now): 
-    
+
+def collect_security_events(now):
     try:
         check_file = open(SECURITY_DIR + "/lastcheck.txt", "r")
     except IOError:
-	# File does not exists, so we create it.
+        # File does not exists, so we create it.
         os.mknod(SECURITY_DIR + "/lastcheck.txt")
         check_file = open(SECURITY_DIR + "/lastcheck.txt", "r")
 
-    last_security_check = datetime.datetime.strptime(now, '%Y%m%d%H%M')
+    last_security_check = datetime.strptime(now, '%Y%m%d%H%M')
     last_check = check_file.read()
     if last_check:
-	last_security_check = datetime.datetime.strptime(last_check, '%Y%m%d%H%M')
-   
+        last_security_check = (
+                            datetime.strptime(last_check, '%Y%m%d%H%M'))
+
     check_file.close()
 
     check_file = open(SECURITY_DIR + "/lastcheck.txt", "w")
     check_file.write(now)
     check_file.close()
-   
-    try:	
-    	csv_file = open(SECURITY_DIR + "/securityevent.csv", "r")
+
+    try:
+        csv_file = open(SECURITY_DIR + "/securityevent.csv", "r")
     except IOError:
-	# File does not exist. No events occured, since last check.
+        # File does not exist. No events occured, since last check.
         return False
 
     data = ""
     for line in csv_file:
-        csv_split = line.split(",")    
-        if datetime.datetime.strptime(csv_split[0], '%Y%m%d%H%M') >= last_security_check:
-	   data += line
+        csv_split = line.split(",")
+        if datetime.strptime(csv_split[0],
+                             '%Y%m%d%H%M') >= last_security_check:
+                data += line
 
     # Check if any new events occured
     if data != "":
-	    securitycheck_file = open(SECURITY_DIR + "/security_check_" + now + ".csv", "w")
-	    securitycheck_file.write(data)	
-	    securitycheck_file.close()
+        securitycheck_file = open(SECURITY_DIR +
+                                  "/security_check_" + now + ".csv", "w")
+        securitycheck_file.write(data)
+        securitycheck_file.close()
 
     csv_file.close()
     os.remove(SECURITY_DIR + "/securityevent.csv")
 
+
 def send_security_events(now):
     (remote_url, uid) = get_url_and_uid()
-    remote = BibOSAdmin(remote_url)    
-    
+    remote = BibOSAdmin(remote_url)
+
     try:
-    	securitycheck_file = open(SECURITY_DIR + "/security_check_" + now + ".csv", "r")
+        securitycheck_file = open(SECURITY_DIR +
+                                  "/security_check_" + now + ".csv", "r")
     except IOError:
-	# File does not exist. No events occured, since last check.
+        # File does not exist. No events occured, since last check.
         return False
 
     csv_data = [line for line in securitycheck_file]
-        
+
     securitycheck_file.close()
-    
+
     try:
         result = remote.push_security_events(uid, csv_data)
-        return result      
+        return result
     except Exception as e:
-        print >> os.sys.stderr, "Error while sending security events:" + str(e)       
+        print >> os.sys.stderr, "Error while sending security events:" + str(e)
         return False
-    
+
 
 def handle_security_events():
-    now = datetime.datetime.now().strftime('%Y%m%d%H%M')
+    now = datetime.now().strftime('%Y%m%d%H%M')
     collect_security_events(now)
     send_security_events(now)
+
 
 def update_and_run():
     try:
