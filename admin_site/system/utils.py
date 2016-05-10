5222678231748f7ce577649112bdb1e635f6a837
dals83 <danni@magenta-aps.dk>
2016-05-10 11:16:09 +0200

    Email message is prettified.

--- admin_site/system/utils.py
+++ admin_site/system/utils.py
@@ -5,18 +5,20 @@ from account.models import UserProfile
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 
-def notify_users(data, securityProblemId):
+def notify_users(data, securityProblem):
     """Notify users about security event.""" 
     
-    #Subject = security type,
-    #Body = technical summary
+    #Subject = security name,
+    #Body = description + technical summary
     email_list = []
-    user_profiles = UserProfile.objects.filter(security_alerts=securityProblemId)
+    user_profiles = UserProfile.objects.filter(security_alerts=securityProblem.id)
     for user in user_profiles:             
         email_list.append(User.objects.get(id=user.user_id).email)
     
+    body = "Beskrivelse af sikkerhedsadvarsel: " + securityProblem.description + "\n"
+    body += "Kort resume af data fra log filen : " + data[2]
     try:
-        message = EmailMessage(data[1], data[4], settings.ADMIN_EMAIL,
+        message = EmailMessage("Sikkerhedsadvarsel: "+securityProblem.name, body, settings.ADMIN_EMAIL,
                                 email_list)
         message.send()
     except Exception:
