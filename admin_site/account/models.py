768e2f5dd5b5f8943292df80ae2e1dc25fb74e9b
Carsten Agger <carstena@magenta.dk>
2016-05-31 10:23:25 +0200

    Fix small bug.

--- admin_site/account/models.py
+++ admin_site/account/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
+from system.models import Site
+
 
 class UserProfile(models.Model):
     """BibOS Admin specific user profile."""
