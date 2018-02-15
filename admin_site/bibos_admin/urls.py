1f35b0f5efa5084d3147d369b8c15b5279a06796
william-magenta <whp@magenta-aps.dk>
2018-02-15 15:12:52 +0100

    urls patterns changed in Django1.11

--- admin_site/bibos_admin/urls.py
+++ admin_site/bibos_admin/urls.py
@@ -1,33 +1,47 @@
 import sys
+#import ipdb
+#from bibos_admin import views
+#from system import views
 
-from django.conf.urls import patterns, include, url
+from django_xmlrpc.views import handle_xmlrpc
+from django.views.static import serve
+
+import django.contrib.auth.views
+from django.conf.urls import include, url
 from django.conf import settings
 
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'bibos_admin.views.home', name='home'),
-    url(r'^accounts/login/', 'django.contrib.auth.views.login',
+    #ipdb.set_trace(),
+#path('accounts/login/', auth_views.LoginView.as_view(template_name='myapp/login.html')),
+    #url('accounts/login/', auth_views.LoginView.as_view(template_name='bibos_admin/login.html')),
+    url('accounts/login/', auth_views.LoginView,
         {'template_name': 'login.html'}, name='login'),
-    url(r'^accounts/logout/', 'django.contrib.auth.views.logout',
+    url(r'^xmlrpc/$', handle_xmlrpc, name='xmlrpc'),
+    url(r'^accounts/logout/', auth_views.LogoutView,
         {'template_name': 'logout.html'}, name='logout'),
     url(r'^', include('system.urls')),
-    url(r'^admin-xml/$', 'django_xmlrpc.views.handle_xmlrpc'),
+    url(r'^admin-xml/$', handle_xmlrpc),
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #ipdb.set_trace(),
+    url('admin/doc/', include('django.contrib.admindocs.urls')),
+#    url(r'^admin/doc/', django.contrib.admindocs.urls),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+]
+
 # Static media when running manually
 if 'runserver' in sys.argv or 'runserver_plus':
-    urlpatterns += patterns(
-        '',
+    urlpatterns += [
         url(
-            r'^media/(.*)$', 'django.views.static.serve',
-            kwargs={'document_root': settings.MEDIA_ROOT}), )
+            r'^media/(.*)$', serve,
+            kwargs={'document_root': settings.MEDIA_ROOT}), 
+]
