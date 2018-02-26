e8427ab067e8376dcc70c6f55f22e856a6cd67e4
william-magenta <whp@magenta-aps.dk>
2018-02-26 10:57:59 +0100

    Directory of system templates has been fixed

--- admin_site/bibos_admin/settings_development.py
+++ admin_site/bibos_admin/settings_development.py
@@ -24,7 +24,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS':
         [
-            '../templates',
+            '../',
+            'templates/'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- admin_site/bibos_admin/settings_production.py
+++ admin_site/bibos_admin/settings_production.py
@@ -25,7 +25,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS':
         [
-            '../templates',
+            '../',
+            'templates/'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
