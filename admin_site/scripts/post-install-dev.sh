9fc7d02c46d00cfeaa5df4a212642e9dcaaf44dd
andreas-magenta <ann@magenta-aps.dk>
2018-04-17 10:38:54 +0200

    Cleanup and settings.py link

--- admin_site/scripts/post-install-dev.sh
+++ admin_site/scripts/post-install-dev.sh
@@ -1,16 +1,23 @@
-#project_root="`pwd`/../"
+#!/usr/bin/env bash
 
-#settings_development.py | sed '/ALLOWED_HOSTS/ s/\[.*]/['"'$4']"'/'
-#ln -s $project_root/bibos_admin/settings_development.py $project_root/bibos_admin/settings.py
+username=$1
+email=$2
+domain=$3
+port=$4
 
-managepy="python $project_root/manage.py"
-user="createsuperuser --username $1 --password $2 --email $3"
-commands=( "makemigrations" "migrate" "$user" "migrate --run-syncdb")
+project_dir=$(dirname `pwd`)
+admin_dir=$project_dir/bibos_admin
+managepy="python $project_dir/manage.py"
+user="createsuperuser --username $username --email $email"
+commands=( "makemigrations" "migrate" $user "migrate --run-syncdb")
+
+$admin_dir/settings_development.py | sed '/ALLOWED_HOSTS/ s/\[.*]/['"'$domain']"'/'
+ln -s $admin_dir/settings_development.py $admin_dir/settings.py
 
 for command in "${commands[@]}"
 do
     $managepy $command
 done
 
-printf "\n\n######\n\nhttp://$4:$5/admin\n\n#####\n\n\n"
-echo "$managepy runserver 0:$5"
+printf "\n\n######\n\nhttp://$domain:$port/admin\n\n#####\n\n\n"
+$managepy runserver 0:$port
