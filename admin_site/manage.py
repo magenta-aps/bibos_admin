08729eeb1da8225622f145520bafa84a814769fc
Andreas Natanael Nielsen <ann@magenta.dk>
2018-05-15 10:38:00 +0200

    BorgerPC is now using an environmnet file

--- admin_site/manage.py
+++ admin_site/manage.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
 if __name__ == "__main__":
+
+    install_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__))
+    )
+
+    dotenv.load_dotenv(install_dir + '/bibos_admin/.env')
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bibos_admin.settings")
 
     from django.core.management import execute_from_command_line
