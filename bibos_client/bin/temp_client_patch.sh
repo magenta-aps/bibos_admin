45285c9c8d0c6c8cc441fcc3cd172661e857836b
Andreas Natanael Nielsen <ann@magenta.dk>
2018-05-31 10:03:24 +0200

    Download direcretly

--- bibos_client/bin/temp_client_patch.sh
+++ bibos_client/bin/temp_client_patch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
+git_url=https://raw.githubusercontent.com/magenta-aps/bibos_admin/feature/22059-update/bibos_client
 bin_folder="/usr/local/bin"
-copy_cmd="sudo cp -v"
 
-$copy_cmd ../bibos_client/admin_client.py /usr/local/lib/python2.7/dist-packages/bibos_client
-$copy_cmd bibos_register_in_admin $bin_folder
-$copy_cmd register_new_bibos_client.sh $bin_folder
+sudo curl $git_url/bibos_client/admin_client.py > /usr/local/lib/python2.7/dist-packages/bibos_client/admin_client.py
+sudo curl $git_url/bin/bibos_register_in_admin > $bin_folder/bibos_register_in_admin
+sudo curl $git_url/bin/register_new_bibos_client.sh > $bin_folder/register_new_bibos_client.sh
