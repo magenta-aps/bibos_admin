ad1f6346ae2ea1387085db5bc7db4e188c2f2a9b
Alexander Faithfull <af@magenta.dk>
2019-02-20 15:52:28 +0100

    Bumped the bibos_utils version number to 0.0.3.1

--- bibos_utils/setup.py
+++ bibos_utils/setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='bibos_utils',
-    version='0.0.3.0',
+    version='0.0.3.1',
     description='Utilities for the BibOS system',
     long_description=readme(),
     classifiers=[
